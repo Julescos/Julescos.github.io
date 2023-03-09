ae1253a225cb71ca10f0616ae6633ce4b6434374
Julescos <126642114+Julescos@users.noreply.github.com>
2023-03-09 12:42:39 +0200

    Changes to mobile version
    Added mobile icon and made changes to mobile version. Link added to contact site.

--- script/main.js
+++ script/main.js
@@ -2,3 +2,14 @@
 const source = document.getElementById('year');
 const year = new Date().getFullYear();
 source.textContent = year;
+
+/* Mobile - onclick icon change style */
+function toggle(x) {
+    x.classList.toggle("change");
+    let y = document.getElementById("list");
+    if (y.style.visibility === "visible" & y.style.opacity === "1") {
+        y.style.visibility = "hidden"; y.style.opacity = "0";
+    } else {
+        y.style.visibility = "visible"; y.style.opacity = "1";
+    }
+}
