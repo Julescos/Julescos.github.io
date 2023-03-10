cab915cac930388abda5d848a8ab887efeb2933e
Julescos <126642114+Julescos@users.noreply.github.com>
2023-03-10 16:05:24 +0200

    Updated main.js

--- script/main.js
+++ script/main.js
@@ -13,9 +13,3 @@ function toggle(x) {
         y.style.visibility = "visible"; y.style.opacity = "1";
     }
 }
-
-/* Google tag (gtag.js) */
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-SXG6E8VL80');
