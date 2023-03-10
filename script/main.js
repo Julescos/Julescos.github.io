d54b28677f967b26934dd62c2a79c19ad82e3362
Julescos <126642114+Julescos@users.noreply.github.com>
2023-03-10 15:54:28 +0200

    Added Google tag

--- script/main.js
+++ script/main.js
@@ -13,3 +13,9 @@ function toggle(x) {
         y.style.visibility = "visible"; y.style.opacity = "1";
     }
 }
+
+/* Google tag (gtag.js) */
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-SXG6E8VL80');
